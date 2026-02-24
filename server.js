8379ecaa126ca5bd8f55dfdc39e8c56d2552f4c0
Ashick Kamaal <kashickkamaal8amat@gmail.com>
2026-02-24 22:49:03 +0530

    Clean project for deployment

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const path = require("path");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const crypto = require("crypto");
 
 const PORT = process.env.PORT || 3000;
